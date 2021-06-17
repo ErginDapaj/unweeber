6793fc735becc6cee11e80a7d4dd6be9c4b2fa27
Grainger <39377884+remixor@users.noreply.github.com>
2021-06-17 12:00:06 +0200

    added some functions lol

--- unweeber.js
+++ unweeber.js
@@ -1,1 +1,19 @@
-The code will be here.
+const human = require('normal-human.js');
+const weeb = require('weird-creature.js');
+const anime = require('JapanesebombforUS.js');
+const dogecoin = require('MuchMoney.js');
+if (weeb.saysanything) {
+  human.deaf()
+} 
+
+if (human.eyes.catch(Anime)) {
+  human.brain.InitiateShutDown()
+}
+
+if (human.ears.catch(Anime)) {
+  human.ears.InitiateDeaf()
+}
+
+if (weeb.transformsInto(human)) {
+  human.reward(dogecoin)
+}
